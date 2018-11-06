607a0bf2cf8aebe02154ea3e8944651231ce5a77
林毅洪 <linyihong@foreveross.com>
2018-11-06 17:16:43 +0800

    2.0

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 $(function(){
-    $(".menu-item").on('click',function(){
-        var type = $(this).attr('type');
-        $(this).addClass('active').siblings().removeClass('active');
-        $(".main").addClass('hidden')
-        $(".main[type="+type+"]").removeClass('hidden');
-    })
+    function initEvents(){
+        $(".menu-item").on('click',function(){
+            var type = $(this).attr('type');
+            $(this).addClass('active').siblings().removeClass('active');
+            $(".main").addClass('hidden')
+            $(".main[type="+type+"]").removeClass('hidden');
+        });
+        // check-in search
+        $('.check-in .search-img').on('click',function(){
+            checkInSearch();
+        })
+    }
+    
+    function checkInSearch(){
+        var val = $('.check-in #check-in-search').val();
+    }
+
+
+
+
+    initEvents();
 })
