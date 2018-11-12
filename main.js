4328e58f100c978d53955b4381e16f28a25ea973
林毅洪 <linyihong@foreveross.com>
2018-11-12 23:18:05 +0800

    1.4

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ $(function(){
     function initEvents(){
         $(".menu-item").on('click',function(){
             var type = $(this).attr('type');
-            $(this).addClass('active').siblings().removeClass('active');
-            $(".main").addClass('hidden')
-            $(".main[type="+type+"]").removeClass('hidden');
+            if(type =='logout'){
+                $('.logout.mask').removeClass('hidden');
+                $(this).addClass('active').siblings().removeClass('active');
+            }else{
+                $(this).addClass('active').siblings().removeClass('active');
+                $(".main").addClass('hidden')
+                $(".main[type="+type+"]").removeClass('hidden');
+                $('.setting-c .menu-c').addClass('hidden');
+                $('.setting .setting-m').removeClass('hidden');
+            }
         });
 
         // check-in search
@@ -16,6 +23,18 @@ $(function(){
         /**setting */
         $(".setting input[type=file]").on('change',function(e){
             $(this).siblings('span').text($(this).val())
+        })
+        $('.setting .setting-m li').on('click',function(){
+            var type = $(this).attr('type');
+            $('.setting .setting-m').addClass('hidden');
+            $(".setting ."+type).removeClass('hidden').siblings().addClass('hidden')
+        })
+        /***logout */
+        $('.logout .mask-btn-yse').on('click',function(){
+            location.href = './login.html'
+        })
+        $('.logout .mask-btn-no').on('click',function(){
+            $('.logout.mask').addClass('hidden');
         })
     }
     
